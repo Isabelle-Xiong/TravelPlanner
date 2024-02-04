afe4293739db316d0326a51bfc5c192dbd49bd12
isabelle-xiong <yeelamisabelle.xiong@duke.edu>
2024-02-04 16:07:39 -0500

    stalled because api payment issue

--- README.old.md
+++
@@ -1,3 +0,0 @@
-# TravelPlanner
-
-testing hi
--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import { divide } from 'lodash';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import { CssBaseline, Grid } from '@material-ui/core';
+
+import Header from './components/Header/Header';
+import List from './components/List/List';
+import Map from './components/Map/Map';
+
+const App = () => {
+
+    return (
+        <>
+            <CssBaseline />
+            <Header />
+            <Grid container spacing={3} style={{ width: '100%' }}>
+                {/* item xs = 12 takes full width on mobile devices, md = 4 takes */}
+                <Grid item xs={12} md={4}></Grid>
+
+            </Grid>
+            <h1>Hello, World!</h1>
+        </>
+    );
+
 }
 
 export default App;
